70547a87b287bef369b958099ebd20241f75e831
peppyguy <31305192+peppyguy@users.noreply.github.com>
2021-04-24 12:17:42 +0530

    Update beagle.py

--- fine-tune-MaskRcnn-master/beagle.py
+++ fine-tune-MaskRcnn-master/beagle.py
@@ -240,7 +240,7 @@ def train(model):
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
                 epochs=1,
-                layers='heads')
+                layers='all')
 
 
 def color_splash(image, mask):
